cc036dc66b7e4c307cb1d11bb2eaa59ec9eae7f1
Mihir Ahlawat <mihiraman@gmail.com>
2019-03-02 15:39:05 +0530

    Update json_parser.py

--- json_parser.py
+++ json_parser.py
@@ -1,10 +1,17 @@
 import json
+import numpy as np
 
 file = 'delhi_highway.json'
 with open(file, "r") as read_file:
     data = json.load(read_file)
 
 coord = []
-
 for line in data['features']:
     coord.append(line['geometry']['coordinates'])
+	
+new = []
+for line in coord:
+    for points in line:
+        new.append(points)
+		
+new_np = np.unique(np.array(new),axis=0)
